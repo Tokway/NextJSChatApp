05d4f9a22b9a97ef81a09744aaf8144d5901cff4
Tokway <linktokway@gmail.com>
2024-04-05 13:40:54 -0700

    Little update

--- app/register/page.js
+++ app/register/page.js
@@ -88,7 +88,7 @@ function page() {
       {/*form*/}
       <form onSubmit={handleSubmit} className="space-y-4 w-full max-w-2xl shadow-lg p-10">
     
-        <h1 className='font-secondary text-xl text-center font-semibold text-[#0b3a65ff]'>CHAT<span className='font-bold text-[#eeab63ff]'>2</span>CHAT</h1>
+        <h1 className='font-secondary text-xl text-center font-semibold text-[#0b3a65ff]'><span className='font-bold text-[#FFFFFF]'>Tokway</span> chat app</h1>
 
         {/* Display the avatar and refresh button */}
         <div className="flex items-center space-y-2 justify-between border border-gray-200 p-2">
